26c9be3edd1df1d02cbeed2af5dda161990da401
JCGrant <jamescolin.grant@gmail.com>
2016-08-04 17:13:59 +0100

    Add pickups.

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+	
+    private Vector3 rotationVelocity = new Vector3(15, 30, 45);
+
+	void Update () {
+        transform.Rotate(rotationVelocity * Time.deltaTime);
+	}
+
+}
