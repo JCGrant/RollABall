76e01b7155d34050df2182f269225cf36875f20a
JCGrant <jamescolin.grant@gmail.com>
2016-08-04 16:45:05 +0100

    Move camera with player.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player;
+    public Vector3 offset;
+
+	void Start () {
+        offset = transform.position - player.transform.position;
+	}
+	
+    // LateUpdate guaranteed to run after all other updates
+	void LateUpdate () {
+        transform.position = player.transform.position + offset;
+	}
+}
